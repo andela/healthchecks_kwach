314f09e9fdd198ec95c7067b879590258b6c4919
ianoti <ianodhiambootieno@gmail.com>
2016-12-13 03:14:12 +0300

    added test asserting number of created profile objects ref[#135592991]

--- hc/accounts/tests/test_team_access_middleware.py
+++ hc/accounts/tests/test_team_access_middleware.py
@@ -9,9 +9,12 @@ class TeamAccessMiddlewareTestCase(TestCase):
         user = User(username="ned", email="ned@example.org")
         user.set_password("password")
         user.save()
+        #returns all Profile Objects with id => 0
+        self.new_number = Profile.objects.filter(user_id__gte = 0)
 
         self.client.login(username="ned@example.org", password="password")
         r = self.client.get("/about/")
         self.assertEqual(r.status_code, 200)
 
         ### Assert the new Profile objects count
+        self.assertEqual(len(self.new_number), 1)
