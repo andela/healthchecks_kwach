a89963cf6bb7416bc698ff168ab7be39d628df11
ianoti <ianodhiambootieno@gmail.com>
2016-12-15 00:08:24 +0300

    removed hardcoding of email check in memeber_emails ref[#135592991]

--- hc/accounts/tests/test_profile.py
+++ hc/accounts/tests/test_profile.py
@@ -53,7 +53,7 @@ class ProfileTestCase(BaseTestCase):
         ### Assert the existence of the member emails
         self.assertEqual(len(member_emails), 2) #Alice included as member
 
-        self.assertTrue("frank@example.org" in member_emails)
+        self.assertTrue(form["email"] in member_emails)
 
         ###Assert that the email was sent and check email content
         self.assertEqual(len(mail.outbox), 1)
