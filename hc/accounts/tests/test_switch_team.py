993067924643b187dafb03f2b9249597e1c574a5
ianoti <ianodhiambootieno@gmail.com>
2016-12-14 08:56:55 +0300

    added status code assertion to test_it_switches ref[#135592991]

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -14,6 +14,7 @@ class SwitchTeamTestCase(BaseTestCase):
         r = self.client.get(url, follow=True)
 
         ### Assert the contents of r
+        self.assertEqual(r.status_code, 200)
         self.assertTemplateUsed(r, "front/my_checks.html")
 
 
